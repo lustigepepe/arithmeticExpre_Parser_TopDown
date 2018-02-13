26202260b3c7db960002bda0595e2178190ffee3
lustigepepe <lustigepepe@web.de>
2018-02-13 21:36:48 +0100

    Initial Commit

---
+++ Parser Task/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Parser Task
+//
+//  Created by soWhat on 13.02.18.
+//  Copyright © 2018 soWhat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
